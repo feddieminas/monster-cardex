5d80fa60bef5bb19939b9cd72a34e6d381f6bf7e
feddieminas <faidon.dermesonoglou@gmail.com>
2019-12-08 12:50:48 +0200

    Updates

---
+++ precache-manifest.08edab374375149e1b0130eb818826ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d630452442517d9c3da05007b295e4f5",
+    "url": "/monster-cardex/index.html"
+  },
+  {
+    "revision": "b08c555079702c0dfd17",
+    "url": "/monster-cardex/static/css/main.c641b7bb.chunk.css"
+  },
+  {
+    "revision": "627a0672e01ef5f1ee0f",
+    "url": "/monster-cardex/static/js/2.b6015e54.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-cardex/static/js/2.b6015e54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b08c555079702c0dfd17",
+    "url": "/monster-cardex/static/js/main.ddaf9c0d.chunk.js"
+  },
+  {
+    "revision": "8d29f0daa549a24cf5b4",
+    "url": "/monster-cardex/static/js/runtime-main.ab30f538.js"
+  }
+]);
